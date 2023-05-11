d9cf4783c1b01a6aa00ad9bbf38e594b4996d067
nffigueroa <nffigueroag@gmail.com>
2023-05-11 13:24:37 -0500

    refactor: remove unused import

--- src/pages/Details.test.tsx
+++ src/pages/Details.test.tsx
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { DetailsPage, HomePage } from '.';
+import { DetailsPage } from '.';
 import { ManufacturerContext } from '../context/manufacturers';
 vi.mock('react-router-dom', () => ({
   useLocation: () => ({ state: { mfId: 957 } }),
